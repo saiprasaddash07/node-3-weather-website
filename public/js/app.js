ed0942875ff76f54b0c749ebb77cc872650186c9
Sai Prasad Dash <sabitadash007@gmail.com>
2020-07-29 12:36:25 +0530

    Add about text in about.hbs

--- public/js/app.js
+++ public/js/app.js
@@ -11,8 +11,8 @@ const messageone = document.querySelector('#message-1');
 const messagetwo = document.querySelector('#message-2');
 //messageone.textContent = 'From js'
 
-weatherForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
+weatherForm.addEventListener('submit',(event)=>{
+    event.preventDefault();
 
     const location = search.value;
     messageone.textContent = 'Loading weather data';
@@ -20,15 +20,15 @@ weatherForm.addEventListener('submit',(e)=>{
 
     //fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
     fetch(`/weather?address=${location}`).then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            return messageone.textContent = data.error ;
-        }else{
-            messageone.textContent = data.location;
-            messagetwo.textContent = data.forecast;
-        }
-    })
-});
+        response.json().then((data)=>{
+            if(data.error){
+                return messageone.textContent = data.error ;
+            }else{
+                messageone.textContent = data.location;
+                messagetwo.textContent = data.forecast;
+            }
+        })
+    });
 })
 
 
